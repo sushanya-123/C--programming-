13d904b3bb17b7c88f8c66672f6e8ae36367ca2b
sushanya-123 <25kq1a5486@pace.ac.in>
2025-11-24 19:53:15 +0530

    Add files via upload

---
+++ neg pos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter a value");
+    scanf("%d",&a);
+    if(a>0)
+       printf("a is positive");
+    else
+       printf("a is negative");  
+    
+    return 0;
+}

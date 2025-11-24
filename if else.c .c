6a188ca6a02d3c47cd7c1a3a36185628ea3a7f25
sushanya-123 <25kq1a5486@pace.ac.in>
2025-11-24 19:00:24 +0530

    Add files via upload

---
+++ if else.c .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("enter the a,b values :");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    printf ("a is big number");
+    else
+    printf ("b is big number");
+    return 0;
+}

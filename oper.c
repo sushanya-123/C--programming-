e07143250458762f14a1ce03dd2bfb571ad1ceed
sushanya-123 <25kq1a5486@pace.ac.in>
2025-11-25 08:32:54 +0530

    Add files via upload

---
+++ oper.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+      int a,b,c;
+      a=100;
+      b=200;
+      c=a>b?a:b;
+      printf("big number=%d",c);
+    
+    return 0;
+}
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    

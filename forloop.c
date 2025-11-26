21b55d5a7aece1184548b068db90e09a63e4d687
sushanya-123 <25kq1a5486@pace.ac.in>
2025-11-26 08:19:40 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main()
+{
+   int i;
+   for(i=1;i<=10;i++)
+      printf("%d ",i);
+   
+      
+    return 0;
+}
+ // method-2
+ /*#include<stdio.h>
+int main()
+{
+    int i;
+    i=1;
+    for(;i<=10;i++) 
+        printf("%d ",i);
+    return 0;    
+}*/
+// method-3
+/*#include<stdio.h>
+ int main()
+ {
+      int i;
+      for(i=1;i<5;printf("%d ",i),i++);
+      printf("%d",i);
+      return 0;   
+      
+ }*/
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    

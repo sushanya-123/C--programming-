88b0573f646cf25d2322b2f60ef2bad28eb5ba8c
sushanya-123 <25kq1a5486@pace.ac.in>
2025-11-26 08:18:37 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+   int i;
+   i=1;
+   do{ 
+       printf ("%d ",i);
+       i++;
+  }
+  while(i<=10);
+   
+      
+    return 0;
+}
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    

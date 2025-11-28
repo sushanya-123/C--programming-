ce02365eb3bbf92fe6b65934f06253b7d1222a26
sushanya-123 <25kq1a5486@pace.ac.in>
2025-11-28 08:17:50 +0530

    Add files via upload

---
+++ auto storage.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{   
+    auto int mainvar;
+    void printnum();
+    mainvar=10;
+    printnum();
+    printf("\n in main is :%d",mainvar);
+}
+ void printnum()
+ {
+    auto int mainvar;
+    printf("in mainvar:%d",mainvar);
+ }
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    

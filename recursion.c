52975bd8d02ff549d5cb1b65c23a8d7096aa19d7
sushanya-123 <25kq1a5486@pace.ac.in>
2025-11-28 07:01:55 +0530

    Add files via upload

---
+++ recursion.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{   
+    int a=0;
+    printf("%d",++a);
+    if(a==1)
+       exit(0);
+     main();  
+}
+ 
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
